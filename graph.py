83c3a4c5b4c18e0962ae0838c1aef98586fe844b
Your Name <you@example.comclear>
2016-11-09 23:13:56 +0530

    this graph is implemented using linked lists

---
+++ graph.py
@@ -0,0 +1,11 @@
+class Vertex:
+    def __init__(self,Data):
+        self.data=Data
+        self.neigbours=None
+        self.others=None
+        
+class Neighbour:
+    def __init__(self,Data):
+        self.data=Data
+        self.next=None
+        
